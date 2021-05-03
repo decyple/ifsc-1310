7f886ad80c51d7c6c5ea970f8ef6ba2318b23f3a
decyple <markamccorkle@yahoo.com>
2021-05-03 20:17:24 +0000

    Added Cycle JS assigmnment

---
+++ cycle/assets/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    	
+    	$('.gallery').cycle({
+            
+            fx: 'fade',
+            speed:  3500
+            
+            //fx:    'scrollDown',
+            //continuous: 1
+            
+        });
+
+});
